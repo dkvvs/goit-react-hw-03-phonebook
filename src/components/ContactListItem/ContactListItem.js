574717153ce4bc6934865e9ede55c58c5797610b
Vitaliy Solomka <dkvvs@ukr.net>
2020-12-19 22:00:08 +0200

    add goit-react-hw-03-phonebook

---
+++ src/components/ContactListItem/ContactListItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import s from './ContactListItem.module.css';
+import { ContactItem } from '../../types';
+
+const ContactListItem = ({ id, name, number, onRemove }) => {
+  return (
+    <li className={s.item}>
+      {name} : {number} <button onClick={() => onRemove(id)}>Delete</button>
+    </li>
+  );
+};
+
+ContactListItem.propTypes = {
+  ...ContactItem,
+  onRemove: PropTypes.func,
+};
+
+export default ContactListItem;
